a4df00a48324dcf24dbd323a3f8c4f8aec897ca8
Sanugi Wickramasinghe <155708370+Sanugiw@users.noreply.github.com>
2025-04-03 23:03:03 +0530

    Delete app.py

--- app.py
+++
@@ -1,7 +0,0 @@
-# Load custom labels from training
-with open('datasets/train/class_indices.json', 'r') as f:
-    class_indices = json.load(f)
-labels = {v:k for k,v in class_indices.items()}
-
-# Load custom model
-interpreter = tflite.Interpreter(model_path="models/trash_model.tflite")
